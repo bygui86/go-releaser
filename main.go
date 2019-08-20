48d4737d255bfd5336919b7e3b98d287c477172c
Matteo Baiguini <matteo@solidarchitectures.com>
2019-08-20 23:22:15 +0200

    Added simple code

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("GoReleaser version 0.0.1")
+	// fmt.Println("GoReleaser version 0.0.2")
+	// fmt.Println("GoReleaser version 0.0.3")
+	// fmt.Println("GoReleaser version 0.0.4")
+	// fmt.Println("GoReleaser version 0.0.5")
+}
